0eac45bca0cb329166bb79062bc51b46b6e28449
kreczko <lkreczko@gmail.com>
2012-10-29 15:39:32 +0000

    added examples for matplotlib plotting (from rootpy) and RooUnfold (from RooUnfold, translated to python)

---
+++ examples/RooUnfoldExample.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# ==============================================================================
+#  File and Version Information:
+#       $Id: RooUnfoldExample.py 302 2011-09-30 20:39:20Z T.J.Adye $
+#
+#  Description:
+#       Simple example usage of the RooUnfold package using toy MC.
+#
+#  Author: Tim Adye <T.J.Adye@rl.ac.uk>
+#
+# ==============================================================================
+
+from ROOT import gRandom, TH1, TH1D, cout, gSystem, TCanvas, TLegend
+gSystem.Load('/software/RooUnfold-1.1.1/libRooUnfold.so')
+from ROOT import RooUnfoldResponse
+from ROOT import RooUnfold
+from ROOT import RooUnfoldBayes
+# from ROOT import RooUnfoldSvd
+# from ROOT import RooUnfoldTUnfold
+from rootpy.plotting import Hist
+import rootpy.plotting.root2matplotlib as rplt
+import matplotlib.pyplot as plt
+from rootpy.utils import asrootpy
+
+
+# ==============================================================================
+#  Gaussian smearing, systematic translation, and variable inefficiency
+# ==============================================================================
+
+def smear(xt):
+    xeff= 0.3 + (1.0-0.3)/20*(xt+10.0);  #  efficiency
+    x= gRandom.Rndm();
+    if x>xeff: 
+        return None
+    xsmear= gRandom.Gaus(-2.5,0.2);     #  bias and smear
+    return xt+xsmear;
+
+# ==============================================================================
+#  Example Unfolding
+# ==============================================================================
+
+print "==================================== TRAIN ===================================="
+response= RooUnfoldResponse (40, -10.0, 10.0);
+
+#  Train with a Breit-Wigner, mean 0.3 and width 2.5.
+for i in xrange(100000):
+    xt= gRandom.BreitWigner (0.3, 2.5);
+    x= smear (xt);
+    if x!=None:
+        response.Fill (x, xt);
+    else:
+        response.Miss (xt);
+
+print "==================================== TEST ====================================="
+hTrue= Hist(40, -10.0, 10.0);
+hMeas= Hist(40, -10.0, 10.0);
+hTrue.SetLineColor('red')
+hMeas.SetLineColor('blue')
+#  Test with a Gaussian, mean 0 and width 2.
+for i in xrange(10000):
+    xt= gRandom.Gaus (0.0, 2.0)
+    x= smear (xt);
+    hTrue.Fill(xt);
+    if x!=None: hMeas.Fill(x);
+
+print "==================================== UNFOLD ==================================="
+unfold= RooUnfoldBayes     (response, hMeas, 4);    #  OR
+# unfold= RooUnfoldSvd     (response, hMeas, 20);   #  OR
+# unfold= RooUnfoldTUnfold (response, hMeas);
+
+hReco= unfold.Hreco();
+h_unfolded = asrootpy(hReco)
+unfold.PrintTable (cout, hTrue);
+
+plt.figure(figsize=(16, 12), dpi=100)
+rplt.hist(hTrue, label='truth', stacked=False)
+rplt.hist(hMeas, label='measured', stacked=False)
+rplt.errorbar(h_unfolded, label='unfolded')
+plt.xlabel('var')
+plt.ylabel('Events')
+plt.title('Unfolding')
+plt.legend()
+plt.savefig('RooUnfoldExample.png')
+print 'Done'
+
+#c = TCanvas("c", 'c', 1600,1200)
+#hReco.SetLineColor(2);
+#hReco.Draw();
+#hMeas.Draw("SAME");
+#hTrue.SetLineColor(8);
+#hTrue.Draw("SAME");
+#leg = Legend.create_legend_in('top right')
+#leg.AddEntry(hReco, 'measured', 'F')
+#leg.AddEntry(hMeas, 'measured after unfolding', 'F')
+#leg.AddEntry(hTrue, 'true', 'F')
+#leg.Draw()
+#c.SaveAs('RooUnfoldExample.png')
---
+++ examples/matplotlib_hist_example.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import numpy as np
+from rootpy.plotting import Hist, HistStack, Legend, Canvas
+import rootpy.plotting.root2matplotlib as rplt
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+import ROOT
+# Setting this to True (default in rootpy)
+# changes how the histograms look in ROOT...
+ROOT.TH1.SetDefaultSumw2(False)
+
+
+# create normal distributions
+mu1, mu2, sigma1, sigma2 = 100, 140, 15, 5
+x1 = mu1 + sigma1 * np.random.randn(10000)
+x2 = mu2 + sigma2 * np.random.randn(1000)
+x1_obs = mu1 + sigma1 * np.random.randn(10000)
+x2_obs = mu2 + sigma2 * np.random.randn(1000)
+
+# create histograms
+h1 = Hist(100, 40, 200, title='Background')
+h2 = h1.Clone(title='Signal')
+h3 = h1.Clone(title='Data')
+h3.markersize=1.2
+
+# fill the histograms with our distributions
+map(h1.Fill, x1)
+map(h2.Fill, x2)
+map(h3.Fill, x1_obs)
+map(h3.Fill, x2_obs)
+
+# set visual attributes
+h1.fillstyle = 'solid'
+h1.fillcolor = 'green'
+h1.linecolor = 'green'
+h1.linewidth = 0
+
+h2.fillstyle = 'solid'
+h2.fillcolor = 'red'
+h2.linecolor = 'red'
+h2.linewidth = 0
+
+stack = HistStack()
+stack.Add(h1)
+stack.Add(h2)
+
+# plot with ROOT
+canvas = Canvas(width=700, height=500)
+canvas.SetLeftMargin(0.15)
+canvas.SetBottomMargin(0.15)
+canvas.SetTopMargin(0.05)
+canvas.SetRightMargin(0.05)
+stack.Draw()
+h3.Draw('E1 same')
+stack.xaxis.SetTitle('Mass')
+stack.yaxis.SetTitle('Events')
+legend = Legend(2)
+legend.AddEntry(h1, 'F')
+legend.AddEntry(h2, 'F')
+legend.AddEntry(h3, 'P')
+legend.Draw()
+canvas.Modified()
+canvas.Update()
+
+# plot with matplotlib
+fig = plt.figure(figsize=(7, 5), dpi=100, facecolor='white')
+axes = plt.axes([0.15, 0.15, 0.8, 0.8])
+axes.xaxis.set_minor_locator(AutoMinorLocator())
+axes.yaxis.set_minor_locator(AutoMinorLocator())
+axes.tick_params(which='major', labelsize=15, length=8)
+axes.tick_params(which='minor', length=4)
+rplt.bar(stack, stacked=True, axes=axes)
+rplt.errorbar(h3, xerr=False, emptybins=False, axes=axes)
+plt.xlabel('Mass', position=(1., 0.), ha='right')
+plt.ylabel('Events', position=(0., 1.), va='top')
+plt.legend(numpoints=1)
+plt.show()
