d0df2c146a9e78b23345e827308485bfc0a824cc
kreczko <lkreczko@gmail.com>
2012-10-30 16:38:54 +0000

    changed creation of asym. bin histogra

--- examples/rootpyAsymBins.py
+++ examples/rootpyAsymBins.py
@@ -4,22 +4,20 @@ Created on 29 Oct 2012
 @author: kreczko
 '''
 from ROOT import TH1F
+from rootpy.plotting import Hist
 import rootpy.plotting.root2matplotlib as rplt
 import matplotlib.pyplot as plt
 from rootpy.utils import asrootpy
 from array import array
 
-#currently only via ROOT histograms
-arglist = array('d', [0, 25, 45, 70, 100, 2000])
-nbins = len(arglist) - 1
-roothist = TH1F('test', 'Title; x-title;y-title', nbins, arglist)
+#updated from http://rootpy.org/qa/questions/80/how-to-create-a-histogram-with-asymmetric-bins
+bins = [0, 25, 45, 70, 100, 2000]
+nbins = len(bins) - 1
 
+rootpyhist = Hist(bins)
 for bin_i in range(nbins):
-    roothist.SetBinContent(bin_i + 1, (bin_i*20 + 25)/arglist[bin_i + 1])
-    roothist.SetBinError(bin_i +1, (bin_i*20 + 25)*0.1/arglist[bin_i + 1])
-
-#make it a rootpy histogram
-rootpyhist = asrootpy(roothist)
+    rootpyhist.SetBinContent(bin_i + 1, bin_i+1)
+    rootpyhist.SetBinError(bin_i +1, (bin_i + 1)*0.1)
 
 plt.figure(figsize=(16, 10), dpi=100)
 plt.figure(1)
@@ -28,5 +26,6 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Testing')
 plt.legend()
+plt.axis([bins[0], bins[-1], 0, nbins*1.2])
 plt.savefig('plots/AsymBinsExample.png')
 print 'Done'
