1011fc26b1b2ed73747d70c86b3a3473b0879814
kreczko <lkreczko@gmail.com>
2012-10-31 14:22:01 +0000

    added RooUnfold configuration for plot layout and for different unfolding methods

---
+++ config/RooUnfold.py
@@ -0,0 +1,24 @@
+'''
+Created on 31 Oct 2012
+
+@author: kreczko
+'''
+
+library = '/software/RooUnfold-1.1.1/libRooUnfold.so'
+
+availablemethods = ['RooUnfoldTUnfold', 'RooUnfoldBayes',
+                        'RooUnfoldSvd','RooUnfoldBinByBin',
+                        'RooUnfoldInvert', 'RooUnfoldTUnfold']
+SVD_k_value = 5
+SVD_n_toy = 1000
+Bayes_n_repeat = 4
+
+unfolded_markerStyle = 20
+unfolded_fillStyle = 0
+unfolded_color = 'black'
+
+truth_color = 'red'
+truth_fillStyle = 0
+
+measured_color = 'blue'
+measured_fillStyle = 0
