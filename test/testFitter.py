1c94b41fb1ffd3388f904d449d1f5158c829b082
kreczko <lkreczko@gmail.com>
2012-11-01 17:22:41 +0000

    added fitter + unit tests

---
+++ test/testFitter.py
@@ -0,0 +1,69 @@
+'''
+Created on 31 Oct 2012
+
+@author: kreczko
+'''
+import unittest
+from tools.Fitter import TMinuitFitter
+from rootpy.plotting import Hist
+
+import numpy as np
+N_bkg1 = 9000
+N_signal = 1000
+N_bkg1_obs = 10000
+N_signal_obs = 2000
+N_data = N_bkg1_obs + N_signal_obs
+mu1, mu2, sigma1, sigma2 = 100, 140, 15, 5
+x1 = mu1 + sigma1 * np.random.randn(N_bkg1)
+x2 = mu2 + sigma2 * np.random.randn(N_signal)
+x1_obs = mu1 + sigma1 * np.random.randn(N_bkg1_obs)
+x2_obs = mu2 + sigma2 * np.random.randn(N_signal_obs)
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        
+
+        # create histograms
+        h1 = Hist(100, 40, 200, title='Background')
+        h2 = h1.Clone(title='Signal')
+        h3 = h1.Clone(title='Data')
+        h3.markersize=1.2
+    
+        # fill the histograms with our distributions
+        map(h1.Fill, x1)
+        map(h2.Fill, x2)
+        map(h3.Fill, x1_obs)
+        map(h3.Fill, x2_obs)
+        
+        histograms = {'signal': h2,
+                      'bkg1': h1,
+                      'data': h3}
+        self.minuitFitter = TMinuitFitter(histograms, dataLabel='data')
+        self.minuitFitter.fit()
+
+
+    def tearDown(self):
+        pass
+
+    def testTemplateKeys(self):
+        templateKeys = sorted(self.minuitFitter.templates.keys())
+        self.assertEqual(templateKeys, sorted(['signal', 'bkg1', 'data']))
+
+    def testNormalisation(self):
+        normalisation = self.minuitFitter.normalisation
+        self.assertAlmostEqual(normalisation["data"], N_data, delta = 1)
+        self.assertAlmostEqual(normalisation["bkg1"], N_bkg1, delta = 1)
+        self.assertAlmostEqual(normalisation["signal"], N_signal, delta = 1)
+        
+    def testSignalResult(self):
+        results = self.minuitFitter.readResults()
+        self.assertAlmostEqual(N_signal_obs, results['signal']['value'], delta = results['signal']['error'])
+        self.assertAlmostEqual(N_bkg1_obs, results['bkg1']['value'], delta = results['bkg1']['error'])
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testTemplates']
+    unittest.main()
---
+++ tools/Fitter.py
@@ -0,0 +1,132 @@
+'''
+Created on 30 Oct 2012
+
+@author: kreczko
+'''
+
+from ROOT import TMinuit, TMath, Long, Double
+from array import array
+
+class Fitter:
+    
+    def __init__(self, histograms, dataLabel='data'):
+        self.performedFit = False
+        self.results = {}
+        self.normalisation = {}
+        self.dataLabel = dataLabel
+        self.histograms = histograms
+        self.templates = {}
+        self.module = None
+        # samples = all inputs except data
+        keys = sorted(histograms.keys())
+        keys.remove(dataLabel)
+        self.samples = keys
+        
+        self.templates, self.normalisation = Fitter.generateTemplatesAndNormalisation(histograms)
+        self.vectors = Fitter.vectorise(self.templates)
+        # create templates
+        
+    @staticmethod
+    def generateTemplatesAndNormalisation(histograms):
+        normalisation = {}
+        templates = {}
+        for sample, histogram in histograms.iteritems():
+            normalisation[sample] = histogram.Integral()
+            temp = histogram.Clone(sample + '_' + 'template')
+            nEntries = temp.Integral()
+            if not nEntries == 0:
+                temp.Scale(1 / nEntries)
+            templates[sample] = temp
+        return templates, normalisation
+            
+    @staticmethod
+    def vectorise(histograms):
+        values = {}
+        for sample in histograms.keys():
+            hist = histograms[sample]
+            nBins = hist.GetNbinsX()
+            for bin_i in range(1, nBins + 1):
+                if not values.has_key(sample):
+                    values[sample] = []
+                values[sample].append(hist.GetBinContent(bin_i))
+        return values
+        
+        
+class TMinuitFitter(Fitter):
+    
+    fitfunction = None
+    
+    def __init__(self, histograms={}, dataLabel='data', method='logLikelihood'):
+        Fitter.__init__(self, histograms, dataLabel)
+        self.method = method
+        
+    def fit(self):
+        numberOfParameters = len(self.samples)
+        gMinuit = TMinuit(numberOfParameters)
+        if self.method == 'logLikelihood':  # set function for minimisation
+            gMinuit.SetFCN(self.logLikelihood)
+        gMinuit.SetPrintLevel(-1)
+        # Error definition: 1 for chi-squared, 0.5 for negative log likelihood
+        gMinuit.SetErrorDef(1)
+        # error flag for functions passed as reference.set to as 0 is no error
+        errorFlag = Long(2)
+        
+        N_total = self.normalisation[self.dataLabel] * 2
+        N_min = 0
+        
+        param_index = 0
+        for sample in self.samples:  # all samples but data
+            gMinuit.mnparm(param_index, sample, self.normalisation[sample], 10.0, N_min, N_total, errorFlag)
+            param_index += 1
+        
+        
+#        N_signal = self.normalisation['signal']
+#        gMinuit.mnparm(0, "N_signal(ttbar+single_top)", N_signal, 10.0, N_min, N_total, errorFlag)
+#        gMinuit.mnparm(1, "bkg1", 10, 10.0, N_min, N_total, errorFlag)
+        
+        arglist = array('d', 10 * [0.])
+        # minimisation strategy: 1 standard, 2 try to improve minimum (a bit slower)
+        arglist[0] = 2
+        # minimisation itself
+        gMinuit.mnexcm("SET STR", arglist, 1, errorFlag)
+        gMinuit.Migrad()
+        self.module = gMinuit
+        self.performedFit = True
+    
+    def logLikelihood(self, nParameters, gin, f, par, iflag):
+        lnL = 0.0
+        data_vector = self.vectors[self.dataLabel]
+        
+        vector_entry = 0
+        for data in data_vector:
+            x_i = 0
+            param_index = 0
+            for sample in self.samples:
+                x_i += par[param_index] * self.vectors[sample][vector_entry]
+                param_index += 1
+            data_i = self.normalisation[self.dataLabel] * data
+            if not data == 0 and not x_i == 0:
+                L = TMath.Poisson(data_i, x_i)
+                lnL += TMath.log(L)
+            
+            
+            
+            vector_entry += 1
+        
+        f[0] = -2.0 * lnL
+        
+    def readResults(self):
+        if not self.module:
+            raise Exception('No fit results available. Please run fit method first')
+        
+        results = {}
+        param_index = 0
+        for sample in self.samples:
+            temp_par = Double(0)
+            temp_err = Double(0)
+            self.module.GetParameter(param_index, temp_par, temp_err)
+            results[sample] = {'value': temp_par, 'error':temp_err}
+            param_index += 1
+        self.results = results
+        return results
+        
