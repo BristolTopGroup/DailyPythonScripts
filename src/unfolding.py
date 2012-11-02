c906252ff9c75a829f4330f35f5570c92ed8765f
kreczko <lkreczko@gmail.com>
2012-11-02 13:54:17 +0000

    updated 2D response histogram input for the in-situ calculation of fakes

--- src/unfolding.py
+++ src/unfolding.py
@@ -20,12 +20,12 @@ inputFile = File('../data/unfolding_merged.root', 'read')
 h_truth = asrootpy(inputFile.unfoldingAnalyserElectronChannel.truth.Rebin(nbins, 'truth', bins))
 h_measured = asrootpy(inputFile.unfoldingAnalyserElectronChannel.measured.Rebin(nbins, 'measured', bins))
 h_fakes = asrootpy(inputFile.unfoldingAnalyserElectronChannel.fake.Rebin(nbins, 'truth', bins))
-h_response = inputFile.unfoldingAnalyserElectronChannel.response_AsymBins
+h_response = inputFile.unfoldingAnalyserElectronChannel.response_withoutFakes_AsymBins #response_AsymBins
 h_truth.Scale(lumiweight)
 h_measured.Scale(lumiweight)
 h_fakes.Scale(lumiweight)
 h_response.Scale(lumiweight)
-#test values
+#test values for real data input
 h_data = Hist(bins.tolist())
 h_data.SetBinContent(1, 2146)
 h_data.SetBinError(1, 145)
@@ -38,9 +38,11 @@ h_data.SetBinError(4, 53)
 h_data.SetBinContent(5, 1722)
 h_data.SetBinError(5, 91)
 
-unfolding = Unfolding(h_truth, h_measured, h_response, h_fakes, method)
-#unfolding.unfold(h_data)
-#unfolding.saveUnfolding('Unfolding_' + method + '.png')
+unfolding = Unfolding(h_truth, h_measured, h_response, method = method)
+#should be identical to
+#unfolding = Unfolding(h_truth, h_measured, h_response, h_fakes, method)
+unfolding.unfold(h_data)
+unfolding.saveUnfolding('Unfolding_' + method + '.png')
 unfolding.closureTest()
 unfolding.saveClosureTest('Unfolding_' + method + '_closure.png')
 
@@ -50,11 +52,11 @@ h_fakes.SetColor('blue')
 fakes.SetFillStyle('\\')
 h_fakes.SetFillStyle('/')
 
+#cross check: are the fakes the same?
 plt.figure(figsize=(16, 10), dpi=100)
 rplt.hist(fakes, label=r'fakes from RM', stacked=False)
 rplt.hist(h_fakes, label=r'fakes from MC', stacked=False, alpha = 0.5)
 plt.xlabel('$E_{\mathrm{T}}^{miss}$')
-#plt.axis([0, 1000, 0, 5000])
 plt.ylabel('Events')
 plt.title('Unfolding')
 plt.legend()
