b8587552379709cf4067dcf14ad66a81e9430c68
kreczko <lkreczko@gmail.com>
2013-01-10 12:20:19 +0000

    fixing log file names

--- src/make_pulls_10k.sh
+++ src/make_pulls_10k.sh
@@ -1,6 +1,6 @@
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 6 &> log_10k_k6 &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 5 &> log_10k_k5 &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 4 &> log_10k_k4 &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 3 &> log_10k_k3 &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 2 &> log_10k_k2 &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 1 &> log_10k_k1 &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 6 &> unfolding_10k_k6.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 5 &> unfolding_10k_k5.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 4 &> unfolding_10k_k4.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 3 &> unfolding_10k_k3.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 2 &> unfolding_10k_k2.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 10000 -k 1 &> unfolding_10k_k1.log &
