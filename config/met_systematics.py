e74f048475c4271c68dad0c51b50a141cbc95ffb
senkin <s.senkin@gmail.com>
2013-03-18 19:34:57 +0000

    MET systematic sources are now in the config folder

---
+++ config/met_systematics.py
@@ -0,0 +1,30 @@
+
+metsystematics_sources = [
+        "patType1CorrectedPFMetElectronEnUp",
+        "patType1CorrectedPFMetElectronEnDown",
+        "patType1CorrectedPFMetMuonEnUp",
+        "patType1CorrectedPFMetMuonEnDown",
+        "patType1CorrectedPFMetTauEnUp",
+        "patType1CorrectedPFMetTauEnDown",
+        "patType1CorrectedPFMetJetResUp",
+        "patType1CorrectedPFMetJetResDown",
+        "patType1CorrectedPFMetJetEnUp",
+        "patType1CorrectedPFMetJetEnDown",
+        "patType1CorrectedPFMetUnclusteredEnUp",
+        "patType1CorrectedPFMetUnclusteredEnDown"
+                      ]
+
+metsystematics_sources_latex = {
+        "patType1CorrectedPFMetElectronEnUp":'Electron energy $+1\sigma$',
+        "patType1CorrectedPFMetElectronEnDown":'Electron energy $-1\sigma$',
+        "patType1CorrectedPFMetMuonEnUp":'Muon energy $+1\sigma$',
+        "patType1CorrectedPFMetMuonEnDown":'Muon energy $-1\sigma$',
+        "patType1CorrectedPFMetTauEnUp":'Tau energy $+1\sigma$',
+        "patType1CorrectedPFMetTauEnDown":'Tau energy $-1\sigma$',
+        "patType1CorrectedPFMetJetResUp":'Jet resolution $+1\sigma$',
+        "patType1CorrectedPFMetJetResDown":'Jet resolution $-1\sigma$',
+        "patType1CorrectedPFMetJetEnUp":'Jet energy $+1\sigma$',
+        "patType1CorrectedPFMetJetEnDown":'Jet energy $-1\sigma$',
+        "patType1CorrectedPFMetUnclusteredEnUp":'Unclustered energy $+1\sigma$',
+        "patType1CorrectedPFMetUnclusteredEnDown":'Unclustered energy $-1\sigma$'
+}
