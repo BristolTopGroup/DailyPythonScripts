350d454acc3d22fcf4a57f327a92761a935cef73
kreczko <lkreczko@gmail.com>
2013-05-14 18:53:12 +0100

    split unfolding driver script into channels

--- src/unfolding_tests/make_pulls_10k_1k.sh
+++ src/unfolding_tests/make_pulls_10k_1k.sh
@@ -1,13 +1,17 @@
-#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 6 &> unfolding_10k_1k_k6.log -c electron &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 5 &> unfolding_10k_1k_k5.log -c electron &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 4 &> unfolding_10k_1k_k4.log -c electron &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 3 &> unfolding_10k_1k_k3.log -c electron &
-#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 2 &> unfolding_10k_1k_k2.log -c electron &
-#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 1 &> unfolding_10k_1k_k1.log -c electron &
+#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 6 -c electron &> unfolding_10k_1k_k6.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 5 -c electron &> unfolding_10k_1k_k5.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 4 -c electron &> unfolding_10k_1k_k4.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 3 -c electron &> unfolding_10k_1k_k3.log &
+#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 2 -c electron &> unfolding_10k_1k_k2.log &
+#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 1 -c electron &> unfolding_10k_1k_k1.log &
 
-#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 6 &> unfolding_10k_1k_k6.log -c muon &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 5 &> unfolding_10k_1k_k5.log -c muon &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 4 &> unfolding_10k_1k_k4.log -c muon &
-nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 3 &> unfolding_10k_1k_k3.log -c muon &
-#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 2 &> unfolding_10k_1k_k2.log -c muon &
-#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 1 &> unfolding_10k_1k_k1.log -c muon &
+#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 6 -c muon &> unfolding_10k_1k_k6.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 5 -c muon &> unfolding_10k_1k_k5.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 4 -c muon &> unfolding_10k_1k_k4.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 3 -c muon &> unfolding_10k_1k_k3.log &
+#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 2 -c muon &> unfolding_10k_1k_k2.log &
+#nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 10000 -e 1000 -k 1 -c muon &> unfolding_10k_1k_k1.log &
+
+
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 100 -e 1000 -k 4 -c electron &> unfolding_10k_1k_k4_first_100.log &
+nohup python unfolding_pull.py -f /storage/TopQuarkGroup/unfolding/unfolding_TTJets_7TeV_toy_mc.root -n 100 -e 1000 -k 4 -c electron -s 100 &> unfolding_10k_1k_k4_first_100.log &
