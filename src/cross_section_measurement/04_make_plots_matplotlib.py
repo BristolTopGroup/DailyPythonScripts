9ef094c9e2b9b38a62f4a27d6076a5aba8c42908
kreczko <lkreczko@gmail.com>
2013-07-05 18:06:44 +0100

    fix: Figures 4-8: - MADGRAPH->MadGraph for consistency with the text.

--- src/cross_section_measurement/04_make_plots_matplotlib.py
+++ src/cross_section_measurement/04_make_plots_matplotlib.py
@@ -304,6 +304,10 @@ def make_plots(histograms, category, output_folder, histname, show_before_unfold
     handles.remove(data_handle)
     labels.insert(0, 'data')
     handles.insert(0, data_handle)
+    
+    #replace MADGPRAPH with MadGraph
+    MADGPRAPH_label_index = labels.index('MADGPRAPH')
+    labels[MADGPRAPH_label_index] = 'MadGraph'
 
     new_handles, new_labels = [], []
     for handle, label in zip(handles, labels):
