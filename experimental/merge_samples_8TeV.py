b8daed70f505edae714d12f43b919f71474a4894
jjacob <jeson.jacob@cern.ch>
2014-05-01 15:40:13 +0100

    Updating scripts for merging histograms files for both 7TeV and 8TeV. Fixed so that maximum 8 jobs run simultaneously.

--- experimental/merge_samples_8TeV.py
+++ experimental/merge_samples_8TeV.py
@@ -3,46 +3,63 @@ import config.cross_section_measurement_8TeV as measurement_config
 
 from tools.file_utilities import merge_ROOT_files
 import os
+import subprocess
+import time
 
 new_files = []
 
 #merge generator systematics histogram files and unfolding ntuples
 for sample, input_samples in sample_summations.iteritems():
-    if not sample in ['QCD_Electron', 'WJets', 'DYJets', 'VJets_matchingup', 'VJets_matchingdown', 'VJets_scaleup', 'VJets_scaledown', 'unfolding_merged', 'unfolding_TTJets_8TeV_mcatnlo', 'unfolding_TTJets_8TeV_powheg', 'unfolding_TTJets_8TeV_matchingup', 'unfolding_TTJets_8TeV_matchingdown', 'unfolding_TTJets_8TeV_scaleup', 'unfolding_TTJets_8TeV_scaledown']: #
+    if not sample in ['WJets', 'DYJets', 'VJets_matchingup', 'VJets_matchingdown', 'VJets_scaleup', 'VJets_scaledown', 'unfolding_merged', 'unfolding_TTJets_8TeV_mcatnlo', 'unfolding_TTJets_8TeV_powheg', 'unfolding_TTJets_8TeV_matchingup', 'unfolding_TTJets_8TeV_matchingdown', 'unfolding_TTJets_8TeV_scaleup', 'unfolding_TTJets_8TeV_scaledown']: #
         continue
     print "Merging"
     if 'unfolding' in sample:
-#	print 'unfolding in sample'
         output_file = measurement_config.unfolding_output_general_template % sample
-	input_files = [measurement_config.unfolding_input_templates[sample] % input_sample for input_sample in input_samples]
+        input_files = [measurement_config.unfolding_input_templates[sample] % input_sample for input_sample in input_samples]
     else: #if any (generator_systematic in sample for generator_systematic in measurement_config.generator_systematics):
-#        print 'generator systematic in sample'
-	output_file = measurement_config.central_general_template % sample
+        output_file = measurement_config.central_general_template % sample
         input_files = [measurement_config.central_general_template % input_sample for input_sample in input_samples]
-
+  
     print output_file
     for input_file in input_files:
         print input_file
+     
     if not os.path.exists(output_file):
         merge_ROOT_files(input_files, output_file, compression = 7)
+        print "merging ", sample
         new_files.append(output_file)
     print '='*120
+     
+    #if 8 concurrent processes, wait until they are finished before starting the next set to avoid overloading the machine
+    number_of_running_hadds = int(subprocess.check_output("ps ax | grep 'hadd' | wc -l", shell=True))
+    while number_of_running_hadds >= 10:
+        time.sleep(30) #sleep for 30 seconds
+        number_of_running_hadds = int(subprocess.check_output("ps ax | grep 'hadd' | wc -l", shell=True))
 
 #merge all other histogram files
 for category in measurement_config.categories_and_prefixes.keys():
     for sample, input_samples in sample_summations.iteritems():
-        if not sample in ['VJets', 'QCD_Muon', 'SingleTop']: #
+        if not sample in ['QCD_Electron', 'QCD_Muon', 'SingleTop', 'VJets']: # 
             continue
         print "Merging"
         output_file = measurement_config.general_category_templates[category] % sample
-        print output_file
         input_files = [measurement_config.general_category_templates[category] % input_sample for input_sample in input_samples]
+        
+        print output_file
         for input_file in input_files:
             print input_file
+        
         if not os.path.exists(output_file):
             merge_ROOT_files(input_files, output_file, compression = 7)
+            print "merging ", category, " ", sample
             new_files.append(output_file)
         print '='*120
+
+        #if 8 concurrent processes, wait until they are finished before starting the next set to avoid overloading the machine
+        number_of_running_hadds = int(subprocess.check_output("ps ax | grep 'hadd' | wc -l", shell=True))
+        while number_of_running_hadds >= 10:
+            time.sleep(30) #sleep for 30 seconds
+            number_of_running_hadds = int(subprocess.check_output("ps ax | grep 'hadd' | wc -l", shell=True))
 
 print '='*120
 print 'Created:'
