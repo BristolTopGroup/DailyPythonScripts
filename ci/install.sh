e49d38466765ff0b46bb5529b502a42db70d987d
Lukasz Kreczko <lkreczko@googlemail.com>
2014-08-01 14:45:05 +0100

    added ENV variable CLANG_VERSION to travis config

--- ci/install.sh
+++ ci/install.sh
@@ -11,7 +11,7 @@ if [[ $TRAVIS_PYTHON_VERSION == '3.2' ]]; then
 	export PYTHON_SUFFIX="3"; 
 fi
 
-# add repositories for gcc 4.8 and clang 3.5
+# add repositories for gcc 4.8 and clang $CLANG_VERSION (set in .travis.yml)
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository --yes 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
@@ -19,7 +19,7 @@ wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 time sudo apt-get update -qq
 
 # Install the dependencies we need
-time sudo apt-get -qq install clang-3.5 libclang-3.5-dev gcc-4.8 g++-4.8
+time sudo apt-get -qq install clang-${CLANG_VERSION} libclang-${CLANG_VERSION}-dev gcc-4.8 g++-4.8
 time sudo apt-get install -qq python${PYTHON_SUFFIX}-numpy python${PYTHON_SUFFIX}-sphinx python${PYTHON_SUFFIX}-nose python${PYTHON_SUFFIX}-pip cython${PYTHON_SUFFIX}
 # matplotlib and PyTables are not available for Python 3 as packages from the main repo yet.
 if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then 
