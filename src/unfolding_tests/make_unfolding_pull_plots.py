d6b3741fa815cd960052cea72be68447021eceac
Lukasz Kreczko <lkreczko@googlemail.com>
2014-08-18 11:45:47 +0100

    fixed bug where 7 TeV pulls where overwritten by 8 TeV

--- src/unfolding_tests/make_unfolding_pull_plots.py
+++ src/unfolding_tests/make_unfolding_pull_plots.py
@@ -231,7 +231,7 @@ if __name__ == "__main__":
     variable = options.variable
     channel = options.channel
     k_value = options.k_value
-    output_folder = options.output_folder + '/' + variable + '/' + channel + '/kv' + str(k_value) + '/'
+    output_folder = options.output_folder + '/' + str(centre_of_mass) + 'TeV/' + variable + '/' + channel + '/kv' + str(k_value) + '/'
     make_folder_if_not_exists(output_folder)
     output_formats = ['pdf']
 
