a82455f316e782ca8e7cabd4fe326fe431c29946
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-05-20 16:32:31 +0100

    Update binning.  Add config which maps simple variable names to their complicated branch names.

---
+++ config/variableBranchNames.py
@@ -0,0 +1,47 @@
+branchNames = {
+	'MT' : 'MT',
+	'WPT' : 'WPT',
+	'leptonPt' : 'leptonPt',
+	'leptonEta' : 'leptonEta',
+	'HT' : 'HT',
+	'ST' : 'ST',
+	'MET' : 'MET',
+	'bEta' : 'bEta',
+	'bPt' : 'bPt',
+
+	'lepTopPt' : 'lepTopPt',
+    'hadTopPt' : 'hadTopPt',
+    'lepTopRap' : 'lepTopRap',
+    'hadTopRap' : 'hadTopRap',
+    'ttbarPt' : 'ttbarPt',
+    'ttbarM' : 'ttbarM',
+    'ttbarRap' : 'ttbarRap',
+
+    'abs_lepTopRap' : 'abs(lepTopRap)',
+    'abs_hadTopRap' : 'abs(hadTopRap)',
+    'abs_ttbarRap' : 'abs(ttbarRap)',
+}
+
+genBranchNames = {
+	'MT' : 'pseudoMT',
+	'WPT' : 'pseudoWPT',
+	'leptonPt' : 'pseudoLepton_pT',
+	'leptonEta' : 'pseudoLepton_eta',
+	'HT' : 'pseudoHT',
+	'ST' : 'pseudoST',
+	'MET' : 'pseudoMET',
+	'bEta' : 'pseudoB_pta',
+	'bPt' : 'pseudoB_pT',
+
+	'lepTopPt' : 'pseudoTop_pT',
+    'hadTopPt' : 'pseudoTop_pT',
+    'lepTopRap' : 'pseudoTop_y',
+    'hadTopRap' : 'pseudoTop_y',
+    'ttbarPt' : 'pseudoTTbar_pT',
+    'ttbarM' : 'pseudoTTbar_m',
+    'ttbarRap' : 'pseudoTTbar_y',
+
+    'abs_lepTopRap' : 'abs(pseudoTop_y)',
+    'abs_hadTopRap' : 'abs(pseudoTop_y)',
+    'abs_ttbarRap' : 'abs(pseudoTTbar_y)',
+}
