96bac5e6f376844e48d56b08f6b80dd0e06c1b9e
kreczko <lkreczko@googlemail.com>
2015-05-26 14:38:31 +0100

    cosmetic and functional changes to 05-make-tables

--- src/cross_section_measurement/05_make_tables.py
+++ src/cross_section_measurement/05_make_tables.py
@@ -1,13 +1,15 @@
 from __future__ import division  # the result of the division will be always a float
 from optparse import OptionParser
 from copy import deepcopy
-from config.latex_labels import variables_latex, measurements_latex, met_systematics_latex, samples_latex
+from config.latex_labels import variables_latex, measurements_latex, met_systematics_latex, samples_latex, typical_systematics_latex
 from config.variable_binning import variable_bins_latex, variable_bins_ROOT
 from config import XSectionConfig
 from tools.Calculation import getRelativeError
 from tools.file_utilities import read_data_from_JSON, make_folder_if_not_exists
 from lib import read_fit_results, read_fit_input
 import math
+import os.path
+from numpy import median
 
 def read_xsection_measurement_results_with_errors(channel):
     global path_to_JSON, variable, k_values, met_type
@@ -357,7 +359,6 @@ def print_error_table(central_values, errors, channel, toFile = True, print_befo
             else:
                 rows[source] = [measurements_latex[source] + ' (\%)', text]
 
-
     header += ' \\\\'
     printout += header
     printout += '\n\\hline\n'
@@ -400,6 +401,148 @@ def print_error_table(central_values, errors, channel, toFile = True, print_befo
         output_file.close()
     else:
         print printout
+        
+def print_typical_systematics_table(central_values, errors, channel, toFile = True, print_before_unfolding = False):
+    global output_folder, variable, k_values, met_type, b_tag_bin, all_measurements
+    bins = variable_bins_ROOT[variable]
+
+    values_for_typical_systematics_table = {}
+    
+    assert(len(bins) == len(errors['central']))
+    if print_before_unfolding:
+        assert(len(bins) == len(central_values['measured']))
+    else:
+        assert(len(bins) == len(central_values['unfolded']))
+    
+    for bin_i, variable_bin in enumerate(bins):
+        if print_before_unfolding:
+            central_value = central_values['measured'][bin_i][0]
+        else:
+            central_value = central_values['unfolded'][bin_i][0]
+
+        for systematic_group in typical_systematics.keys():
+            for source in all_measurements:
+                if source in typical_systematics[systematic_group]:
+                    abs_error = errors[source][bin_i]
+                    relative_error = getRelativeError(central_value, abs_error)
+                    value = relative_error
+                    if values_for_typical_systematics_table.has_key(source):
+                        values_for_typical_systematics_table[source].append(value)
+                    else:
+                        values_for_typical_systematics_table[source] = [typical_systematics_latex[systematic_group] + ' (\%)', value]
+    
+    rows_for_typical_systematics_table = {}
+    
+    if variable == "HT":
+        del(typical_systematics["typical_systematics_MET"])
+    for systematic_group in typical_systematics.keys():
+        typical_systematics_row = []
+        typical_systematics_row.append(typical_systematics_latex[systematic_group] + ' (\%)')
+        for bin_i, variable_bin in enumerate(bins):
+            sum = 0.
+            
+            #if only one systematic in the group, in each bin just use the systematic value (absolute)
+            if len(typical_systematics[systematic_group]) == 1:
+                sum += abs(values_for_typical_systematics_table[typical_systematics[systematic_group][0]][bin_i+1])
+            
+            #if two systematics in the group, in each bin use the largest of the two absolute values
+            elif len(typical_systematics[systematic_group]) == 2:
+                low_syst = abs(values_for_typical_systematics_table[typical_systematics[systematic_group][0]][bin_i+1])
+                high_syst = abs(values_for_typical_systematics_table[typical_systematics[systematic_group][1]][bin_i+1])
+                sum += max(low_syst, high_syst)
+            
+            #if more than two systematics in the group, take the maximum of each pair of up/down systematics, and add these maximum values for all systematics in quadrature
+            else:
+                for systematic in typical_systematics [systematic_group]:
+                    index_in_group = typical_systematics[systematic_group].index(systematic)
+                    if index_in_group %2 == 1 : #if not 0th, 2nd, 4th etc. element of list, get out of loop
+                        continue
+                    else:
+                        low_syst = abs(values_for_typical_systematics_table[typical_systematics[systematic_group][index_in_group]][bin_i+1])
+                        high_syst = abs(values_for_typical_systematics_table[typical_systematics[systematic_group][index_in_group+1]][bin_i+1])
+                        largest = max(low_syst, high_syst) #(values_for_typical_systematics_table[systematic][bin_i+1])
+                        sum += pow(largest, 2)
+                sum = math.sqrt(sum)
+
+            typical_systematics_row.append(sum)
+        label = typical_systematics_row.pop(0)
+        
+        #for each systematic group, take the median of the values across all bins 
+        value = median(typical_systematics_row)
+        text = '%.2f' % (value*100)
+        rows_for_typical_systematics_table[systematic_group] = [label, text]
+
+    printout = '%% ' + '=' * 60
+    printout += '\n'
+    printout += '%% Typical systematics table for %s channel, k-value %s, met type %s, %s b-tag region\n' % (channel, str(k_values[channel]), met_type, b_tag_bin)
+    if print_before_unfolding:
+        printout += '%% BEFORE UNFOLDING\n'
+    printout += '%% ' + '=' * 60
+    printout += '\n'
+
+    printout += '\\begin{table}[htbp]\n'
+    printout += '\\centering\n'
+    printout += '\\caption{Typical systematic uncertainties (median values) for the normalised \\ttbar cross section measurement \n'
+    printout += 'at a centre-of-mass energy of %d TeV ' % measurement_config.centre_of_mass_energy
+    if channel == 'combined':
+        printout += '(combination of electron and muon channels).}\n'
+    else:
+        printout += '(%s channel).}\n' % channel
+    printout += '\\label{tab:typical_systematics_%dTeV_%s}\n' % (measurement_config.centre_of_mass_energy, channel)
+    printout += '\\resizebox{\\columnwidth}{!} {\n'
+    printout += '\\begin{tabular}{l' + 'r'*len(variable_bins_ROOT) + '}\n'
+    printout += '\\hline\n'
+    
+    header = 'Uncertainty source '
+    header += '& %s' % (variables_latex[variable])
+
+    header += ' '
+    printout += header
+    printout += '\n\\hline\n'
+
+    for systematic_group in sorted(rows_for_typical_systematics_table.keys()):
+        if systematic_group == 'central':
+            continue
+        for item in rows_for_typical_systematics_table[systematic_group]:
+            printout += item + ' & '
+        printout = printout.rstrip('& ')
+        printout += ' \n'
+
+    printout += '\\hline \n'
+    printout += '\\hline \n'
+    printout += '\\end{tabular}\n'
+    printout += '}\n'
+    printout += '\\end{table}\n'
+
+    if toFile:
+        path = output_folder + '/'  + str(measurement_config.centre_of_mass_energy) + 'TeV/'
+        make_folder_if_not_exists(path)
+        file_template = path + '/typical_systematics_%dTeV_%s.tex' % (measurement_config.centre_of_mass_energy, channel)
+
+        if print_before_unfolding:
+            make_folder_if_not_exists(path + '/before_unfolding/')
+            file_template = file_template.replace(path, path + '/before_unfolding/')
+        if os.path.isfile(file_template): 
+            with open(file_template, 'r+') as output_file:
+                lines = output_file.readlines()
+                for line_number, line in enumerate (lines):
+                    if line.startswith("Uncertainty source"):
+                        lines[line_number] = lines[line_number].strip() + "& " + variables_latex[variable] + "\n"
+                    elif variable == "HT" and line.startswith("$E_{T}^{miss}$ uncertainties"):
+                        lines[line_number] = lines[line_number].strip() + "& - \n"
+                    else:
+                        for table_entry in enumerate(typical_systematics_latex):
+                            if line.startswith(typical_systematics_latex[table_entry[1]]):
+                                lines[line_number] = lines[line_number].strip() + "& "  + rows_for_typical_systematics_table[table_entry[1]][1] + " \n"
+                output_file.seek(0)
+                for line in lines:
+                    output_file.write(line)            
+        else:
+            output_file = open(file_template, 'w')
+            output_file.write(printout)
+        output_file.close()
+    else:
+        print printout
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -425,6 +568,7 @@ if __name__ == '__main__':
     ttbar_theory_systematic_prefix = measurement_config.ttbar_theory_systematic_prefix
     vjets_theory_systematic_prefix = measurement_config.vjets_theory_systematic_prefix
     met_systematics_suffixes = measurement_config.met_systematics_suffixes
+    typical_systematics = measurement_config.typical_systematics
 
     variable = options.variable
     output_folder = options.output_folder
@@ -457,13 +601,16 @@ if __name__ == '__main__':
     #kValue_systematics = measurement_config.kValueSystematic
     #categories.extend( measurement_config.kValueSystematic )
 
-    # all MET uncertainties except JES as this is already included
-    met_uncertainties = [met_type + suffix for suffix in met_systematics_suffixes if not 'JetEn' in suffix and not 'JetRes' in suffix]
-    new_uncertainties = ['hadronisation', 'QCD_shape', 'PDF_total_lower', 'PDF_total_upper']
+    # all MET uncertainties except JES as this is already included, and only if variable is not HT, since MET is not used in HT
+    if not variable == "HT":
+        met_uncertainties = [met_type + suffix for suffix in met_systematics_suffixes if not 'JetEn' in suffix and not 'JetRes' in suffix]
+    
+    new_uncertainties = ['hadronisation', 'ptreweight_max', 'QCD_shape', 'PDF_total_lower', 'PDF_total_upper']
     rate_changing_systematics = [systematic + '+' for systematic in measurement_config.rate_changing_systematics.keys()]
     rate_changing_systematics.extend([systematic + '-' for systematic in measurement_config.rate_changing_systematics.keys()])
     all_measurements = deepcopy(categories)
-    all_measurements.extend(met_uncertainties)
+    if not variable == "HT":
+        all_measurements.extend(met_uncertainties)
     all_measurements.extend(new_uncertainties)
     all_measurements.extend(rate_changing_systematics)
 
@@ -475,6 +622,10 @@ if __name__ == '__main__':
 
         print_error_table(normalised_xsection_measured_unfolded, normalised_xsection_unfolded_errors, channel, toFile = True, print_before_unfolding = False)
         print_error_table(normalised_xsection_measured_unfolded, normalised_xsection_measured_errors, channel, toFile = True, print_before_unfolding = True)
+
+        if channel == 'combined':
+            print_typical_systematics_table(normalised_xsection_measured_unfolded, normalised_xsection_unfolded_errors, channel, toFile = True, print_before_unfolding = False)
+#             print_typical_systematics_table(normalised_xsection_measured_unfolded, normalised_xsection_measured_errors, channel, toFile = True, print_before_unfolding = True)
 
         if not channel == 'combined':
             fit_input = read_fit_input(path_to_JSON, variable, 'central', channel, met_type)
