8a4ef1e25cd87ade3db52740163241acce89d5ba
kreczko <lkreczko@googlemail.com>
2015-07-16 17:46:14 +0100

    first draft of condor submission for DPS

---
+++ condor/jobtypes/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from .create_toy_mc_job import CreateToyMCJob
+from .unfolding_pull_job import UnfoldingPullJob
+
+__all__ = [
+    'CreateToyMCJob',
+    'UnfoldingPullJob',
+]
