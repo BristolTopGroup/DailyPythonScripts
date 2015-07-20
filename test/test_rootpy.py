f76ca574936b19550daa293a74083c765a9feca5
kreczko <lkreczko@googlemail.com>
2015-07-20 14:59:09 +0100

    updated rootpy to fix directory path bug

---
+++ test/test_rootpy.py
@@ -0,0 +1,40 @@
+'''
+Created on 21 Jul 2015
+
+@author: phxlk
+'''
+import unittest
+from test.data import create_test_hist, create_test_tree
+from rootpy.io.file import Directory, File
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        f = File('test.root', 'recreate')
+        f.mkdir('TTbar_plus_X_analysis/EPlusJets/Ref selection', recurse=True)
+        f.cd('TTbar_plus_X_analysis/EPlusJets/Ref selection')
+        tree = create_test_tree()
+        h = create_test_hist()
+        h.write()
+        tree.write()
+        f.write()
+        f.Close()
+
+
+    def tearDown(self):
+        pass
+
+
+    def testDirectoryWithSpace(self):
+        '''
+            Reported and fixed:
+            https://github.com/rootpy/rootpy/issues/663
+        '''
+        with File.open('test.root') as f:
+            self.assertTrue(f.__contains__('TTbar_plus_X_analysis/EPlusJets/Ref selection'))
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
