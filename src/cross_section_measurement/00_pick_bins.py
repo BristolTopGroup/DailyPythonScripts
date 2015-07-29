35cb4cc4f01cbf1dae4f97702c513d08e705bb1a
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-07-29 11:31:36 +0100

    Fix 00_pick_bins in one more place.

--- src/cross_section_measurement/00_pick_bins.py
+++ src/cross_section_measurement/00_pick_bins.py
@@ -202,14 +202,14 @@ def get_best_binning( histogram_information, p_min, s_min, n_min, x_min = None )
         get_bin_content = new_hist.ProjectionX().GetBinContent
         purities = calculate_purities( new_hist.Clone() )
         stabilities = calculate_stabilities( new_hist.Clone() )
-        n_events = [int( new_hist.ProjectionX().GetBinContent( i ) ) for i in range( 1, len( bin_edges ) )]
+        n_events = [int( get_bin_content( i ) ) for i in range( 1, len( bin_edges ) )]
         # Now check if the last bin also fulfils the requirements
         if purities[-1] < p_min or stabilities[-1] < s_min or n_events[-1] < n_min:
             # if not, merge last two bins 
             bin_edges[-2] = bin_edges[-1]
             bin_edges = bin_edges[:-1]
             new_hist = rebin_2d( info['hist'], bin_edges, bin_edges ).Clone()
-            get_bin_content = new_hist.GetBinContent
+            get_bin_content = new_hist.ProjectionX().GetBinContent
             purities = calculate_purities( new_hist.Clone() )
             stabilities = calculate_stabilities( new_hist.Clone() )
             n_events = [int( get_bin_content( i ) ) for i in range( 1, len( bin_edges ) )]
