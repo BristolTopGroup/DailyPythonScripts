66af711808f99fa20cfb8168028fcfc807e41928
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-08-13 13:39:24 +0100

    Update05 to work with background subtraction.

--- src/cross_section_measurement/05_make_tables.py
+++ src/cross_section_measurement/05_make_tables.py
@@ -2,7 +2,7 @@ from __future__ import division  # the result of the division will be always a f
 from optparse import OptionParser
 from copy import deepcopy
 from config.latex_labels import variables_latex, measurements_latex, samples_latex, typical_systematics_latex, met_systematics_latex
-from config.variable_binning import variable_bins_latex, variable_bins_ROOT
+from config.variable_binning import variable_bins_latex, variable_bins_ROOT, variable_bins_visiblePS_ROOT, variable_bins_visiblePS_latex
 from config import XSectionConfig
 from tools.Calculation import getRelativeError
 from tools.file_utilities import read_data_from_JSON, make_folder_if_not_exists
@@ -12,10 +12,10 @@ import os.path
 from numpy import median
 
 def read_xsection_measurement_results_with_errors(channel):
-    global path_to_JSON, variable, met_type, phaseSpaceSuffix
+    global path_to_JSON, variable, met_type, phase_space
     category = 'central'
 
-    file_template = path_to_JSON + '/' + variable +  '/xsection_measurement_results_' + phaseSpaceSuffix + '/' + channel + '/' + category + '/normalised_xsection_' + met_type + '.txt' 
+    file_template = path_to_JSON +  '/xsection_measurement_results/' + channel + '/' + category + '/normalised_xsection_' + met_type + '.txt' 
 
     file_name = file_template
     normalised_xsection_unfolded = read_data_from_JSON( file_name )
@@ -72,8 +72,15 @@ def read_xsection_measurement_results_with_errors(channel):
     return normalised_xsection_measured_unfolded, normalised_xsection_measured_errors, normalised_xsection_unfolded_errors
 
 def print_fit_results_table(initial_values, fit_results, channel, toFile = True):
-    global output_folder, variable, met_type
-    bins = variable_bins_ROOT[variable]
+    global output_folder, variable, met_type, phase_space
+    bins = None
+    bins_latex = None
+    if phase_space == 'VisiblePS':
+        bins = variable_bins_visiblePS_ROOT[variable]
+        bins_latex = variable_bins_visiblePS_latex
+    elif phase_space == 'FullPS':
+        bins = variable_ROOT[variable]
+        bins_latex = variable_bins_latex
 
     printout = '%% ' + '=' * 60
     printout += '\n'
@@ -132,7 +139,7 @@ def print_fit_results_table(initial_values, fit_results, channel, toFile = True)
     N_fit_sum_MC_error = 0
 
     for bin_i, variable_bin in enumerate(bins):
-        header += ' & %s' % (variable_bins_latex[variable_bin])
+        header += ' & %s' % (bins_latex[variable_bin])
         ttjet_in_line += ' & %.1f $\pm$ %.1f' % (initial_values['TTJet'][bin_i][0], initial_values['TTJet'][bin_i][1])
         N_initial_ttjet += initial_values['TTJet'][bin_i][0]
         N_initial_ttjet_error += initial_values['TTJet'][bin_i][1]
@@ -235,7 +242,7 @@ def print_fit_results_table(initial_values, fit_results, channel, toFile = True)
         print printout
 
 def print_xsections(xsections, channel, toFile = True, print_before_unfolding = False):
-    global output_folder, variable, met_type, b_tag_bin
+    global output_folder, variable, met_type, b_tag_bin, phase_space
     printout = '%% ' + '=' * 60
     printout += '\n'
     printout += '%% Results for %s variable, %s channel, met type %s, %s b-tag region\n' % (variable, channel, met_type, b_tag_bin)
@@ -262,7 +269,17 @@ def print_xsections(xsections, channel, toFile = True, print_before_unfolding = 
     printout += '\\\\ \n\hline\n'
     scale = 1000
     
-    bins = variable_bins_ROOT[variable]
+    bins = None
+    bins_latex = None
+    if phase_space == 'VisiblePS':
+        bins = variable_bins_visiblePS_ROOT[variable]
+        bins_latex = variable_bins_visiblePS_latex
+    elif phase_space == 'FullPS':
+        bins = variable_ROOT[variable]
+        bins_latex = variable_bins_latex
+
+    print phase_space
+    print bins
     assert(len(bins) == len(xsections['unfolded_with_systematics']))
     
     for bin_i, variable_bin in enumerate(bins):
@@ -279,10 +296,10 @@ def print_xsections(xsections, channel, toFile = True, print_before_unfolding = 
         total_relativeError_up = getRelativeError(value, total_error_up)
         total_relativeError_down = getRelativeError(value, total_error_down)
         if total_error_up == total_error_down:
-            printout += '%s & ' % variable_bins_latex[variable_bin] + ' $%.2f$ & $ \pm~ %.2f^\\dagger$ & $ \pm~ %.2f^\\star$ & ' % (value * scale, stat_error * scale, syst_error_up * scale) +\
+            printout += '%s & ' % bins_latex[variable_bin] + ' $%.2f$ & $ \pm~ %.2f^\\dagger$ & $ \pm~ %.2f^\\star$ & ' % (value * scale, stat_error * scale, syst_error_up * scale) +\
                     '$(%.2f' % (total_relativeError_up * 100) + '\%)$'
         else:
-            printout += '%s & ' % variable_bins_latex[variable_bin] + ' $%.2f$ & $ \pm~ %.2f^\\dagger$ & $ ~^{+%.2f}_{-%.2f}^\\star$ & ' % (value * scale, stat_error * scale, syst_error_up * scale, syst_error_down * scale) +\
+            printout += '%s & ' % bins_latex[variable_bin] + ' $%.2f$ & $ \pm~ %.2f^\\dagger$ & $ ~^{+%.2f}_{-%.2f}^\\star$ & ' % (value * scale, stat_error * scale, syst_error_up * scale, syst_error_down * scale) +\
                     '$(^{+%.2f}_{-%.2f}' % (total_relativeError_up * 100, total_relativeError_down * 100) + '\%)$'
         printout += '\\\\ \n'
 
@@ -306,8 +323,15 @@ def print_xsections(xsections, channel, toFile = True, print_before_unfolding = 
         print printout
 
 def print_error_table(central_values, errors, channel, toFile = True, print_before_unfolding = False):
-    global output_folder, variable, met_type, b_tag_bin, all_measurements, phaseSpaceSuffix
-    bins = variable_bins_ROOT[variable]
+    global output_folder, variable, met_type, b_tag_bin, all_measurements, phase_space
+    bins = None
+    bins_latex = None
+    if phase_space == 'VisiblePS':
+        bins = variable_bins_visiblePS_ROOT[variable]
+        bins_latex = variable_bins_visiblePS_latex
+    elif phase_space == 'FullPS':
+        bins = variable_ROOT[variable]
+        bins_latex = variable_bins_latex
 
     printout = '%% ' + '=' * 60
     printout += '\n'
@@ -343,7 +367,7 @@ def print_error_table(central_values, errors, channel, toFile = True, print_befo
         assert(len(bins) == len(central_values['unfolded']))
     
     for bin_i, variable_bin in enumerate(bins):
-        header += '& %s' % (variable_bins_latex[variable_bin])
+        header += '& %s' % (bins_latex[variable_bin])
         if print_before_unfolding:
             central_value = central_values['measured'][bin_i][0]
         else:
@@ -409,8 +433,12 @@ def print_error_table(central_values, errors, channel, toFile = True, print_befo
         print printout
         
 def print_typical_systematics_table(central_values, errors, channel, toFile = True, print_before_unfolding = False):
-    global output_folder, variable, k_values, met_type, b_tag_bin, all_measurements
-    bins = variable_bins_ROOT[variable]
+    global output_folder, variable, k_values, met_type, b_tag_bin, all_measurements, phase_space
+    bins = None
+    if phase_space == 'VisiblePS':
+        bins = variable_bins_visiblePS_ROOT[variable]
+    elif phase_space == 'FullPS':
+        bins = variable_ROOT[variable]
 
     values_for_typical_systematics_table = {}
     
@@ -496,7 +524,7 @@ def print_typical_systematics_table(central_values, errors, channel, toFile = Tr
         printout += '(%s channel).}\n' % channel
     printout += '\\label{tab:typical_systematics_%dTeV_%s}\n' % (measurement_config.centre_of_mass_energy, channel)
     printout += '\\resizebox{\\columnwidth}{!} {\n'
-    printout += '\\begin{tabular}{l' + 'r'*len(variable_bins_ROOT) + '}\n'
+    printout += '\\begin{tabular}{l' + 'r'*len(bins) + '}\n'
     printout += '\\hline\n'
     
     header = 'Uncertainty source '
@@ -581,15 +609,21 @@ if __name__ == '__main__':
     variable = options.variable
     output_folder = options.output_folder
 
+    visiblePS = options.visiblePS
+    phase_space = 'FullPS'
+    if visiblePS:
+        phase_space = 'VisiblePS'
+    output_folder += '/' + str(measurement_config.centre_of_mass_energy) + 'TeV/' + phase_space + '/'
+
+    print phase_space
     met_type = translate_options[options.metType]
     b_tag_bin = translate_options[options.bjetbin]
-    path_to_JSON = options.path + '/' + str(measurement_config.centre_of_mass_energy) + 'TeV/'
 
-    visiblePS = options.visiblePS
-    phaseSpaceSuffix = 'FullPS'
-    if visiblePS:
-        phaseSpaceSuffix = 'VisiblePS'
-    output_folder += '/' + str(measurement_config.centre_of_mass_energy) + 'TeV/' + phaseSpaceSuffix + '/'
+    path_to_JSON = '{path}/{com}TeV/{variable}/{phase_space}/'
+    path_to_JSON = path_to_JSON.format(path = options.path, com = options.CoM,
+                                       variable = variable,
+                                       phase_space = phase_space,
+                                       )
 
     #remove btag mistagging rate systematic - new btagging method has only one, all-inclusive sytematic
     categories_and_prefixes = measurement_config.categories_and_prefixes
