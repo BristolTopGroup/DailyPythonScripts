8d1a2b7abf6da16d5ac5fbd2aa44ec693f245cc5
jjacob <jeson.jacob@cern.ch>
2015-08-25 10:39:42 +0100

    Updates to scripts for merging latest BAT output files on DICE.

--- experimental/mergeJob.sh
+++ experimental/mergeJob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scramv1 project CMSSW CMSSW_6_2_12
-cd CMSSW_6_2_12/src
+scramv1 project CMSSW CMSSW_7_4_7
+cd CMSSW_7_4_7/src
 eval `scramv1 runtime -sh`
 tar -xf ../../dps.tar
 cd DailyPythonScripts/
@@ -8,7 +8,7 @@ cd external/
 rm -r vpython/
 cd ../
 git submodule init && git submodule update
-./setup_standalone.sh
+./setup.sh
 eval `scramv1 runtime -sh`
 . environment.sh
 python experimental/merge_samples_onDICE.py -n $1 -c $2
