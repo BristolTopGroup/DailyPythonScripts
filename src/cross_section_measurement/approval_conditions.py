fdd623bd9dd0a1a06a879127614d39f55bdf8a31
kreczko <lkreczko@googlemail.com>
2015-09-23 12:06:52 +0100

    more plots

--- src/cross_section_measurement/approval_conditions.py
+++ src/cross_section_measurement/approval_conditions.py
@@ -6,33 +6,41 @@ from tools.plotting import Histogram_properties, compare_histograms, Plot, \
     ErrorBand
 from tools.file_utilities import read_data_from_JSON
 from tools.hist_utilities import value_error_tuplelist_to_hist,\
-    clean_control_region, absolute
+    clean_control_region, absolute, value_tuplelist_to_hist
 from config.variable_binning import bin_edges_vis
 from config.latex_labels import variables_latex
 from tools.ROOT_utils import get_histogram_from_tree
 from config.cross_section_config import XSectionConfig
 
 
-def compare_unfolding_methods():
+def compare_unfolding_methods(measurement='normalised_xsection',
+                              add_before_unfolding=False):
     file_template = 'data/normalisation/background_subtraction/13TeV/'
     file_template += '{variable}/VisiblePS/central/'
-    file_template += 'unfolded_normalisation_combined_RooUnfold{method}.txt'
+    file_template += '{measurement}_combined_RooUnfold{method}.txt'
 
     variables = ['MET', 'HT', 'ST', 'NJets',
                  'lepton_pt', 'abs_lepton_eta', 'WPT']
 #     variables = ['ST']
     for variable in variables:
-        svd = file_template.format(variable=variable, method='Svd')
-        bayes = file_template.format(variable=variable, method='Bayes')
-        svd_data = read_data_from_JSON(svd)['TTJet_unfolded']
+        svd = file_template.format(
+            variable=variable, method='Svd', measurement=measurement)
+        bayes = file_template.format(
+            variable=variable, method='Bayes', measurement=measurement)
+        data = read_data_from_JSON(svd)
+        before_unfolding = data['TTJet_measured']
+        svd_data = data['TTJet_unfolded']
         bayes_data = read_data_from_JSON(bayes)['TTJet_unfolded']
         h_svd = value_error_tuplelist_to_hist(
             svd_data, bin_edges_vis[variable])
         h_bayes = value_error_tuplelist_to_hist(
             bayes_data, bin_edges_vis[variable])
+        h_before_unfolding = value_error_tuplelist_to_hist(
+            before_unfolding, bin_edges_vis[variable])
 
         properties = Histogram_properties()
-        properties.name = 'compare_unfolding_methods_{0}'.format(variable)
+        properties.name = '{0}_compare_unfolding_methods_{1}'.format(
+            measurement, variable)
         properties.title = 'Comparison of unfolding methods'
         properties.path = 'plots'
         properties.has_ratio = True
@@ -40,10 +48,17 @@ def compare_unfolding_methods():
         properties.x_limits = (
             bin_edges_vis[variable][0], bin_edges_vis[variable][-1])
         properties.x_axis_title = variables_latex[variable]
-        properties.y_axis_title = r'$\frac{1}{\sigma}  \frac{d\sigma}{d' + \
-            variables_latex[variable] + '}$'
+        if 'xsection' in measurement:
+            properties.y_axis_title = r'$\frac{1}{\sigma}  \frac{d\sigma}{d' + \
+                variables_latex[variable] + '}$'
+        else:
+            properties.y_axis_title = r'$t\bar{t}$ normalisation'
 
         histograms = {'SVD': h_svd, 'Bayes': h_bayes}
+        if add_before_unfolding:
+            histograms['before unfolding'] = h_before_unfolding
+            properties.name += '_ext'
+            properties.has_ratio = False
         plot = Plot(histograms, properties)
         plot.draw_method = 'errorbar'
         compare_histograms(plot)
@@ -124,7 +139,7 @@ def compare_QCD_control_regions_to_MC():
             }
             inputs['tree'] = mc
             h_qcd = get_histogram_from_tree(input_file=qcd_file, **inputs)
-    
+
             h_ctrl1 = clean_control_region(
                 hs_ctrl1,
                 data_label='data',
@@ -140,18 +155,19 @@ def compare_QCD_control_regions_to_MC():
             n_data1 = h_ctrl1.integral()
             n_data2 = h_ctrl2.integral()
             n_qcd_sg = h_qcd.integral()
-    
+
             ratio_ctrl1 = n_data1 / n_qcd_ctrl1
             ratio_ctrl2 = n_data2 / n_qcd_ctrl2
             qcd_estimate_ctrl1 = n_qcd_sg * ratio_ctrl1
             qcd_estimate_ctrl2 = n_qcd_sg * ratio_ctrl2
             h_ctrl1.Scale(qcd_estimate_ctrl1 / n_data1)
             h_ctrl2.Scale(qcd_estimate_ctrl2 / n_data2)
-    
+
             properties = Histogram_properties()
             properties.name = 'compare_qcd_control_regions_to_mc_{0}_{1}_channel'.format(
                 variable, channel)
-            properties.title = 'Comparison of QCD control regions ({0} channel)'.format(channel)
+            properties.title = 'Comparison of QCD control regions ({0} channel)'.format(
+                channel)
             properties.path = 'plots'
             properties.has_ratio = False
             properties.xerr = True
@@ -159,7 +175,7 @@ def compare_QCD_control_regions_to_MC():
                 bin_edges_vis[variable][0], bin_edges_vis[variable][-1])
             properties.x_axis_title = variables_latex[variable]
             properties.y_axis_title = 'number of QCD events'
-    
+
             histograms = {'control region 1': h_ctrl1,
                           'control region 2': h_ctrl2,
                           'MC prediction': h_qcd}
@@ -172,6 +188,58 @@ def compare_QCD_control_regions_to_MC():
             plot_e.add_error_band(err_e)
             compare_histograms(plot_e)
 
+
+def compare_unfolding_uncertainties():
+    file_template = 'data/normalisation/background_subtraction/13TeV/'
+    file_template += '{variable}/VisiblePS/central/'
+    file_template += 'unfolded_normalisation_combined_RooUnfold{method}.txt'
+
+    variables = ['MET', 'HT', 'ST', 'NJets',
+                 'lepton_pt', 'abs_lepton_eta', 'WPT']
+#     variables = ['ST']
+    for variable in variables:
+        svd = file_template.format(
+            variable=variable, method='Svd')
+        bayes = file_template.format(
+            variable=variable, method='Bayes')
+        data = read_data_from_JSON(svd)
+        before_unfolding = data['TTJet_measured']
+        svd_data = data['TTJet_unfolded']
+        bayes_data = read_data_from_JSON(bayes)['TTJet_unfolded']
+
+        before_unfolding = [e / v * 100 for v, e in before_unfolding]
+        svd_data = [e / v * 100 for v, e in svd_data]
+        bayes_data = [e / v * 100 for v, e in bayes_data]
+
+        h_svd = value_tuplelist_to_hist(
+            svd_data, bin_edges_vis[variable])
+        h_bayes = value_tuplelist_to_hist(
+            bayes_data, bin_edges_vis[variable])
+        h_before_unfolding = value_tuplelist_to_hist(
+            before_unfolding, bin_edges_vis[variable])
+
+        properties = Histogram_properties()
+        properties.name = 'compare_unfolding_uncertainties_{0}'.format(
+            variable)
+        properties.title = 'Comparison of unfolding uncertainties'
+        properties.path = 'plots'
+        properties.has_ratio = False
+        properties.xerr = True
+        properties.x_limits = (
+            bin_edges_vis[variable][0], bin_edges_vis[variable][-1])
+        properties.x_axis_title = variables_latex[variable]
+        properties.y_axis_title = 'relative uncertainty (\\%)'
+        properties.legend_location = (0.98, 0.95)
+
+        histograms = {'SVD': h_svd, 'Bayes': h_bayes,
+                      'before unfolding': h_before_unfolding}
+        plot = Plot(histograms, properties)
+        plot.draw_method = 'errorbar'
+        compare_histograms(plot)
+
 if __name__ == '__main__':
-    compare_unfolding_methods()
+    compare_unfolding_methods('normalised_xsection')
+    compare_unfolding_methods('normalised_xsection', add_before_unfolding=True)
+    compare_unfolding_methods('unfolded_normalisation')
     compare_QCD_control_regions_to_MC()
+    compare_unfolding_uncertainties()
