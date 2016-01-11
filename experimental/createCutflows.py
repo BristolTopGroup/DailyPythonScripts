4a4154c4a10c68134d028ce028948b3177d10f45
Douglas Burns <db0268@bristol.ac.uk>
2016-01-11 15:32:03 +0000

    Add SFWeight plots & Simplify Cutflow plots

--- experimental/createCutflows.py
+++ experimental/createCutflows.py
@@ -12,22 +12,32 @@ import glob
 if __name__ == '__main__':
 
 	basepath = '/hdfs/TopQuarkGroup/run2/ntuples/25ns/v6/'
-	outpath = '/plots/CutFlows/'
-	if not os.path.exists(outpath):
-		os.makedirs(outpath)
+	outpath = '../plots/CutFlows/'
 
-
+	# if not os.path.exists(outpath): 
+	# 	os.makedirs(outpath)
 
 	mu_consec = TH1F('muConsecCut', 'MuPlusJets Consectutive Cutflow', 11, -0.500000, 10.500000)
 	mu_indiv = TH1F('muIndivCut', 'MuPlusJets Individual Cutflow', 11, -0.500000, 10.500000)
 	e_consec = TH1F('eConsecCut', 'EPlusJets Consectutive Cutflow', 12, -0.500000, 11.500000)
 	e_indiv = TH1F('eIndivCut', 'EPlusJets Individual Cutflow', 12, -0.500000, 11.500000)
 
+	tt_mu_consec = TH1F('tt_muConsecCut', 'MC MuPlusJets Consectutive Cutflow', 11, -0.500000, 10.500000)
+	tt_mu_indiv = TH1F('tt_muIndivCut', 'MC MuPlusJets Individual Cutflow', 11, -0.500000, 10.500000)
+	tt_e_consec = TH1F('tt_eConsecCut', 'MC EPlusJets Consectutive Cutflow', 12, -0.500000, 11.500000)
+	tt_e_indiv = TH1F('tt_eIndivCut', 'MC EPlusJets Individual Cutflow', 12, -0.500000, 11.500000)
+
 	electron_datasets = []
 	muon_datasets = []
+	tt_datasets = []
+
+	muon_plots = [ mu_consec , mu_indiv , tt_mu_consec, tt_mu_indiv ]
+	electron_plots = [ e_consec , e_indiv , tt_e_consec, tt_e_indiv ]
+
 	for dirs in os.listdir(basepath):
 		if "Electron" in dirs : electron_datasets.append(dirs)
 		if "Muon" in dirs : muon_datasets.append(dirs)
+		if dirs == "TTJets_PowhegPythia8" : tt_datasets.append(dirs)
 
 	for dataset in muon_datasets:
 		datasetpath = basepath+dataset+'/'
@@ -93,88 +103,88 @@ if __name__ == '__main__':
 
 
 
-	mu_consec.GetXaxis().SetBinLabel(1, "Total Events");
-	mu_consec.GetXaxis().SetBinLabel(2, "Event Cleaning and Trigger");
-	mu_consec.GetXaxis().SetBinLabel(3, "ExactlyOneSignalMuon");
-	mu_consec.GetXaxis().SetBinLabel(4, "LooseMuonVeto");
-	mu_consec.GetXaxis().SetBinLabel(5, "LooseElectronVeto");
-	mu_consec.GetXaxis().SetBinLabel(6, "AtLeastOneGoodJet");
-	mu_consec.GetXaxis().SetBinLabel(7, "AtLeastTwoGoodJets");
-	mu_consec.GetXaxis().SetBinLabel(8, "AtLeastThreeGoodJets");
-	mu_consec.GetXaxis().SetBinLabel(9, "AtLeastFourGoodJets");
-	mu_consec.GetXaxis().SetBinLabel(10, "AtLeastOneBTag");
-	mu_consec.GetXaxis().SetBinLabel(11, "AtLeastTwoBTags");
-	mu_consec.GetYaxis().SetTitle("Number of Events");
+	for dataset in tt_datasets:
+		datasetpath = basepath+dataset+'/'
+		print datasetpath	
 
-	e_consec.GetXaxis().SetBinLabel(1, "Total Events");
-	e_consec.GetXaxis().SetBinLabel(2, "Event Cleaning and Trigger");
-	e_consec.GetXaxis().SetBinLabel(3, "ExactlyOneSignalElectron");
-	e_consec.GetXaxis().SetBinLabel(4, "LooseMuonVeto");
-	e_consec.GetXaxis().SetBinLabel(5, "LooseElectronVeto");
-	e_consec.GetXaxis().SetBinLabel(6, "ConversionVeto");
-	e_consec.GetXaxis().SetBinLabel(7, "AtLeastOneGoodJet");
-	e_consec.GetXaxis().SetBinLabel(8, "AtLeastTwoGoodJets");
-	e_consec.GetXaxis().SetBinLabel(9, "AtLeastThreeGoodJets");
-	e_consec.GetXaxis().SetBinLabel(10, "AtLeastFourGoodJets");
-	e_consec.GetXaxis().SetBinLabel(11, "AtLeastOneBTag");
-	e_consec.GetXaxis().SetBinLabel(12, "AtLeastTwoBTags");
-	e_consec.GetYaxis().SetTitle("Number of Events");
+		rootfiles = []
+		for files in os.listdir(datasetpath):
+			rootfiles.append(files)
 
-	mu_indiv.GetXaxis().SetBinLabel(1, "Total Events");
-	mu_indiv.GetXaxis().SetBinLabel(2, "Event Cleaning and Trigger");
-	mu_indiv.GetXaxis().SetBinLabel(3, "ExactlyOneSignalMuon");
-	mu_indiv.GetXaxis().SetBinLabel(4, "LooseMuonVeto");
-	mu_indiv.GetXaxis().SetBinLabel(5, "LooseElectronVeto");
-	mu_indiv.GetXaxis().SetBinLabel(6, "AtLeastOneGoodJet");
-	mu_indiv.GetXaxis().SetBinLabel(7, "AtLeastTwoGoodJets");
-	mu_indiv.GetXaxis().SetBinLabel(8, "AtLeastThreeGoodJets");
-	mu_indiv.GetXaxis().SetBinLabel(9, "AtLeastFourGoodJets");
-	mu_indiv.GetXaxis().SetBinLabel(10, "AtLeastOneBTag");
-	mu_indiv.GetXaxis().SetBinLabel(11, "AtLeastTwoBTags");
-	mu_indiv.GetYaxis().SetTitle("Number of Events");
+		for f in rootfiles:
+			rootfile = datasetpath+f			
+			if not os.path.exists(rootfile): 
+				print 'File Does Not exist'
+				break
+			else: print 'Adding File : ', rootfile
 
-	e_indiv.GetXaxis().SetBinLabel(1, "Total Events");
-	e_indiv.GetXaxis().SetBinLabel(2, "Event Cleaning and Trigger");
-	e_indiv.GetXaxis().SetBinLabel(3, "ExactlyOneSignalElectron");
-	e_indiv.GetXaxis().SetBinLabel(4, "LooseMuonVeto");
-	e_indiv.GetXaxis().SetBinLabel(5, "LooseElectronVeto");
-	e_indiv.GetXaxis().SetBinLabel(6, "ConversionVeto");
-	e_indiv.GetXaxis().SetBinLabel(7, "AtLeastOneGoodJet");
-	e_indiv.GetXaxis().SetBinLabel(8, "AtLeastTwoGoodJets");
-	e_indiv.GetXaxis().SetBinLabel(9, "AtLeastThreeGoodJets");
-	e_indiv.GetXaxis().SetBinLabel(10, "AtLeastFourGoodJets");
-	e_indiv.GetXaxis().SetBinLabel(11, "AtLeastOneBTag");
-	e_indiv.GetXaxis().SetBinLabel(12, "AtLeastTwoBTags");
-	e_indiv.GetYaxis().SetTitle("Number of Events");
+			rootfile = TFile(rootfile)
 
+			hist_consec_mu = rootfile.Get("topPairMuPlusJetsSelectionAnalyser/consecutiveCuts")
+			hist_indiv_mu = rootfile.Get("topPairMuPlusJetsSelectionAnalyser/individualCuts")
+			hist_consec_e = rootfile.Get("topPairEPlusJetsSelectionAnalyser/consecutiveCuts")
+			hist_indiv_e = rootfile.Get("topPairEPlusJetsSelectionAnalyser/individualCuts")
 
-	mu_Canvas_consec = TCanvas("muCutFlow_consec","MuCutFlow", 0, 0, 800, 600)
-	mu_Canvas_consec.SetLogy()
-	mu_Canvas_consec.SetGridy()
-	mu_consec.Draw()
-	mu_Canvas_consec.Update()
-	mu_Canvas_consec.SaveAs(outpath+'MuPlusJets_CutFlow_Consecutive.pdf')
+			clone_consec_mu = hist_consec_mu.Clone()
+			clone_consec_mu.SetDirectory(0)
+			clone_indiv_mu = hist_indiv_mu.Clone()
+			clone_indiv_mu.SetDirectory(0)
+			clone_consec_e = hist_consec_e.Clone()
+			clone_consec_e.SetDirectory(0)
+			clone_indiv_e = hist_indiv_e.Clone()
+			clone_indiv_e.SetDirectory(0)
 
-	e_Canvas_consec = TCanvas("eCutFlow_consec","ECutFlow", 0, 0, 800, 600)
-	e_Canvas_consec.SetLogy()
-	e_Canvas_consec.SetGridy()
-	e_consec.Draw()
-	e_Canvas_consec.Update()
-	e_Canvas_consec.SaveAs(outpath+'EPlusJets_CutFlow_Consecutive.pdf')
+			rootfile.Close()
 
-	mu_Canvas_indiv = TCanvas("muCutFlow_indiv","MuCutFlow", 0, 0, 800, 600)
-	mu_Canvas_indiv.SetLogy()
-	mu_Canvas_indiv.SetGridy()
-	mu_indiv.Draw()
-	mu_Canvas_indiv.Update()
-	mu_Canvas_indiv.SaveAs(outpath+'MuPlusJets_CutFlow_Individual.pdf')
+			tt_mu_consec.Add(clone_consec_mu)
+			tt_mu_indiv.Add(clone_indiv_mu)
+			tt_e_consec.Add(clone_consec_e)
+			tt_e_indiv.Add(clone_indiv_e)
 
-	e_Canvas_indiv = TCanvas("eCutFlow_indiv","ECutFlow", 0, 0, 800, 600)
-	e_Canvas_indiv.SetLogy()
-	e_Canvas_indiv.SetGridy()
-	e_indiv.Draw()
-	e_Canvas_indiv.Update()
-	e_Canvas_indiv.SaveAs(outpath+'EPlusJets_CutFlow_Individual.pdf')
+	for plot in muon_plots :
+		plot.GetXaxis().SetBinLabel(1, "Total Events");
+		plot.GetXaxis().SetBinLabel(2, "Event Cleaning and Trigger");
+		plot.GetXaxis().SetBinLabel(3, "ExactlyOneSignalMuon");
+		plot.GetXaxis().SetBinLabel(4, "LooseMuonVeto");
+		plot.GetXaxis().SetBinLabel(5, "LooseElectronVeto");
+		plot.GetXaxis().SetBinLabel(6, "AtLeastOneGoodJet");
+		plot.GetXaxis().SetBinLabel(7, "AtLeastTwoGoodJets");
+		plot.GetXaxis().SetBinLabel(8, "AtLeastThreeGoodJets");
+		plot.GetXaxis().SetBinLabel(9, "AtLeastFourGoodJets");
+		plot.GetXaxis().SetBinLabel(10, "AtLeastOneBTag");
+		plot.GetXaxis().SetBinLabel(11, "AtLeastTwoBTags");
+		plot.GetYaxis().SetTitle("Number of Events");
 
+	for plot in electron_plots :
+		plot.GetXaxis().SetBinLabel(1, "Total Events");
+		plot.GetXaxis().SetBinLabel(2, "Event Cleaning and Trigger");
+		plot.GetXaxis().SetBinLabel(3, "ExactlyOneSignalElectron");
+		plot.GetXaxis().SetBinLabel(4, "LooseMuonVeto");
+		plot.GetXaxis().SetBinLabel(5, "LooseElectronVeto");
+		plot.GetXaxis().SetBinLabel(6, "ConversionVeto");
+		plot.GetXaxis().SetBinLabel(7, "AtLeastOneGoodJet");
+		plot.GetXaxis().SetBinLabel(8, "AtLeastTwoGoodJets");
+		plot.GetXaxis().SetBinLabel(9, "AtLeastThreeGoodJets");
+		plot.GetXaxis().SetBinLabel(10, "AtLeastFourGoodJets");
+		plot.GetXaxis().SetBinLabel(11, "AtLeastOneBTag");
+		plot.GetXaxis().SetBinLabel(12, "AtLeastTwoBTags");
+		plot.GetYaxis().SetTitle("Number of Events");
 
+	for plot in electron_plots :
+		canvas = TCanvas("CanvasRef", "CanvasTitle", 800,600)
+		canvas.SetLogy()
+		canvas.SetGridy()
+		plot.Draw()
+		canvas.Update()
+		canvas.SaveAs(outpath+plot.GetTitle()+'.pdf')	
+		canvas.Close()	
+
+	for plot in muon_plots :
+		canvas = TCanvas("CanvasRef", "CanvasTitle", 800,600)
+		canvas.SetLogy()
+		canvas.SetGridy()
+		plot.Draw()
+		canvas.Update()
+		canvas.SaveAs(outpath+plot.GetTitle()+'.pdf')	
+		canvas.Close()	
 
