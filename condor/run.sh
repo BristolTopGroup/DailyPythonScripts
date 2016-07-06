0c68cdd346fccb4bf2022eca9e8a0792d67eb9ef
Lukasz Kreczko <lkreczko@googlemail.com>
2016-07-06 11:53:53 +0100

    updated condor scripts

--- condor/run.sh
+++ condor/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cmssw_version=CMSSW_7_4_7
-git_branch=master
-echo "Setting up ${cmssw_version} ..."
-echo "... sourcing CMS default environment from CVMFS"
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-echo "... creating CMSSW project area"
-scramv1 project CMSSW ${cmssw_version}
-cd ${cmssw_version}/src
-eval `scramv1 runtime -sh`
-echo "${cmssw_version} has been set up"
+export CONDAINSTALL=/software
+export PATH="$CONDAINSTALL/miniconda/bin:$PATH"
+export ENVNAME=dps-new
+export CONDA_ENV_PATH=$CONDAINSTALL/miniconda/envs/$ENVNAME
+
+source activate dps-new # This version comes with ROOT 6.04
+git clone https://github.com/BristolTopGroup/DailyPythonScripts
+cd DailyPythonScripts
+export PYTHONPATH=$PYTHONPATH:`pwd`
+export PATH=$PATH:$base/bin
 
 echo "Setting up DailyPythonScripts from tar file ..."
 echo "... getting ${git_branch} branch"
@@ -16,16 +16,10 @@ echo "... getting ${git_branch} branch"
 time git clone https://github.com/BristolTopGroup/DailyPythonScripts.git
 cd DailyPythonScripts/
 git checkout ${git_branch}
-echo "... setting up git submodules"
->&2 echo "... setting up git submodules"
-time git submodule init && git submodule update
 echo "... copying dps.tar from hdfs"
 hadoop fs -copyToLocal $5/dps.tar ${_CONDOR_JOB_IWD}/dps.tar
 echo "... extracting ${_CONDOR_JOB_IWD}/dps.tar on top"
 tar -xf ${_CONDOR_JOB_IWD}/dps.tar --overwrite
-echo "... enforcing conda python environment"
-# this is safe, as the dangerous part is only executed on soolin
-source environment_conda.sh
 echo "DailyPythonScripts are set up"
 
 echo "Running payload"
--- condor/setup_dps_from_tar.sh
+++ condor/setup_dps_from_tar.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 echo "Setting up DailyPythonScripts from tar file ..."
-echo "... getting master branch"
-git clone https://github.com/BristolTopGroup/DailyPythonScripts.git
+echo "... getting ${git_branch} branch"
+>&2 echo "... getting ${git_branch} branch"
+time git clone https://github.com/BristolTopGroup/DailyPythonScripts.git
 cd DailyPythonScripts/
-echo "... setting up git submodules"
-git submodule init && git submodule update
-echo "... running setup routine"
-./setup.sh
-echo "... enforcing virtual python environment"
-source environment.sh
+git checkout ${git_branch}
+echo "... copying dps.tar from hdfs"
+hadoop fs -copyToLocal $5/dps.tar ${_CONDOR_JOB_IWD}/dps.tar
 echo "... extracting ${_CONDOR_JOB_IWD}/dps.tar on top"
 tar -xf ${_CONDOR_JOB_IWD}/dps.tar --overwrite
 echo "DailyPythonScripts are set up"
